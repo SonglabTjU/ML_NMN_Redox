01709e7e4cffd639291caffe0d5df1aeca363cca
Song_LAB <125991295+SonglabTjU@users.noreply.github.com>
2024-11-21 15:51:46 +0800

    ML_NMN_Redox_XgBoost.py

--- ML_NMN_Redox_XgBoost.py
+++ ML_NMN_Redox_XgBoost.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 # In[2]:
 
-file_path = r'filename2.csv'
+file_path = r'FileS2.csv'
 data = pd.read_csv(file_path, encoding='gbk')
 X=data.iloc[:,1:4].values
 y=data.iloc[:,-1].values
